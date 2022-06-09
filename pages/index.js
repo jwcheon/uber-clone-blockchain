ecf84ff0d173ae5b6e4e566add83c2c640f863d4
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-06-10 00:28:40 +0900

    Create home div structure

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
+// Separate 'style' b/c Tailwind CSS can get messy.
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`
+}
 
 export default function Home() {
-  return <div></div>
+  return (
+    <div className={style.wrapper}>
+      {/* navbar */}
+      <div className={style.main}>
+        {/* map */}
+      </div>
+      <div className={style.rideRequestContainer}>
+        <div className={style.rideRequest}> 
+          {/* location selector */}
+          {/* confirm ride */}
+        </div>
+      </div>
+    </div>
+  )
 }
+
+
+// <div className="h-sc h-scree w-sc"></div>
